b4cb3e59feadda57bd153a3e34b41e4ae6379d64
MeralTd <meraltasdemir77@gmail.com>
2021-04-20 22:27:27 +0300

    Refactored entity framework package

--- CineApp.DataAccess/Concrete/EntityFramework/Contexts/MovieDbContext.cs
+++ CineApp.DataAccess/Concrete/EntityFramework/Contexts/MovieDbContext.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CineApp.Core.Concrete.EntityFramework.Contexts
 {
-    public class MovieDbContext /*: DbContext*/
+    public class MovieDbContext : DbContext
     {
         ////public DbSet<Category> Categories { get; set; }
 
