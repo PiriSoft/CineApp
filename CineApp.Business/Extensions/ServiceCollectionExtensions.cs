34ea2a02b63da9ff8296c7a396b2e7669fa300a3
MeralTd <meraltasdemir77@gmail.com>
2021-04-21 16:07:08 +0300

    Add migration

---
+++ CineApp.Business/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using CineApp.Core.Concrete.EntityFramework.Contexts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CineApp.Business.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<MovieDbContext>();
+           
+
+            return serviceCollection;
+        }
+    }
+}
